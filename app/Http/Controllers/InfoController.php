d85857c63f10b0881dc653f2cdff349c1ab6bf9c
unknown <muminovxurshidbek/2gmail.com>
2023-05-01 21:49:34 +0500

    paginate added

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -7,12 +7,16 @@ use Illuminate\Http\Request;
 
 class InfoController extends Controller
 {
-   
+
     public function index()
     {
-        $infos = Info::orderBy('id', 'DESC')->get();
+        $infos = Info::orderBy('id', 'DESC')->paginate(3);
 
         return view('admin.infos.index', compact('infos'));
+    }
+
+    public function create(){
+        return view('admin.infos.create');
     }
 
     public function store(Request $request)
@@ -21,12 +25,12 @@ class InfoController extends Controller
 
         return redirect()->route('admin.infos.index');
     }
-    
+
     public function show(Info $info)
     {
         return view('admin.infos.show', compact('info'));
     }
-    
+
     public function edit(Info $info)
     {
         return view('admin.infos.edit', compact('info'));
--- resources/views/admin/infos/index.blade.php
+++ resources/views/admin/infos/index.blade.php
@@ -53,6 +53,7 @@
 							@endforeach
 						</tbody>
 					</table>
+                    {{$infos->links()}}
 
 				</div>
 
